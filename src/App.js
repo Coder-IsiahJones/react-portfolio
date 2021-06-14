fa1bcd782f2d589f7945d9f87746744077466c27
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-14 01:24:50 -0400

    Add call Stack.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
 import Nav from "./components/Nav"
+import Stack from "./components/Stack"
 
 function App() {
   return (
     <div>
       <Nav />
       <Header />
+      <Stack />
     </div>
   )
 }
