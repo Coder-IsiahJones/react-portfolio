50714a02fb608a98a39b901cfa47925d3e083334
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-14 03:40:20 -0400

    Add call Project.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./components/Header"
 import Nav from "./components/Nav"
 import Stack from "./components/Stack"
+import Project from "./components/Project"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Header />
       <Stack />
+      <Project />
     </div>
   )
 }
