5b4631b303576a07bdca7fa8a4cf4fbaf7b9a882
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-14 14:49:46 -0400

    Add Aos package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Header from "./components/Header"
 import Nav from "./components/Nav"
 import Stack from "./components/Stack"
 import Project from "./components/Project"
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 function App() {
+  useEffect(() => {
+    Aos.init({})
+  }, [])
+
   return (
     <div>
       <Nav />
